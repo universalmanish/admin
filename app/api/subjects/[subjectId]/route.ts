2c0189a20a359dc725e3b41486ca43ccb1e38915
Manish Chaudhary <126310496+universalmanish@users.noreply.github.com>
2024-04-23 16:41:41 +0000

    new files

---
+++ app/api/subjects/[subjectId]/route.ts
@@ -0,0 +1,25 @@
+import db from "@/db/drizzle"
+import { subject } from "@/db/schema"
+import { eq } from "drizzle-orm"
+import { NextResponse } from "next/server"
+
+export const GET = async (req: Request, {params}: {params: {subjectId: number}}) => {
+    const data = await db.query.subject.findFirst({
+        where: eq(subject.id, params.subjectId)
+    })
+    return NextResponse.json(data)
+}
+
+export const PUT = async (req: Request, {params}: {params: {subjectId: number}}) => {
+    const body = await req.json()
+    const data = await db.update(subject).set({
+        ...body,
+    }).where(eq(subject.id, params.subjectId)).returning()
+
+    return NextResponse.json(data)
+}
+
+export const DELETE = async (req: Request, {params}: {params: {subjectId: number}}) => {
+    const data = await db.delete(subject).where(eq(subject.id, params.subjectId)).returning()
+    return NextResponse.json(data)
+}
--- components/App.tsx
+++ components/App.tsx
@@ -3,9 +3,11 @@ import { BranchList } from "@/app/branch/list";
 import { HeadingList } from "@/app/heading/list";
 import { LevelList } from "@/app/level/list";
 import { SubHeadingList } from "@/app/subheading/list";
+
 import { SubjectCreate } from "@/app/subjects/create";
 import { SubjectEdit } from "@/app/subjects/edit";
 import { SubjectList } from "@/app/subjects/list";
+
 import simpleRestProvider from "ra-data-simple-rest";
 import { Admin, Resource } from "react-admin";
 
